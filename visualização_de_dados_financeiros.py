e55e92325f35b848aaaa6a8ea638d69f681f36e0
Luiz Cunha - Maverick <97998057+lj1981@users.noreply.github.com>
2024-12-23 19:14:59 -0300

    Update visualização_de_dados_financeiros.py

--- visualização_de_dados_financeiros.py
+++ visualização_de_dados_financeiros.py
@@ -12,7 +12,7 @@ visualização de dados financeiros
 
 import pandas as pd
 import numpy as np
-from pandas_datareader import data
+#from pandas_datareader import data
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
