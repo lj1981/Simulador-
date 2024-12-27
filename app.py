eb192374a5bdb656367c8556c7475a28b3495848
Luiz Cunha - Maverick <97998057+lj1981@users.noreply.github.com>
2024-12-27 15:20:40 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+hosts = ['100.20.92.101', '44.255.181.72', '44.227.217.144']
+port = int(os.environ.get('PORT', 5000))
+
+while True:
+    for host in hosts:
+        app.run(host=host, port=port)
+        time.sleep(60)  # Pausar por 60 segundos antes de trocar de host
