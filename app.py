7d6470caa61faaaff6699edd7dceb4987f516ac9
Luiz Cunha - Maverick <97998057+lj1981@users.noreply.github.com>
2024-12-27 15:30:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 hosts = ['100.20.92.101', '44.255.181.72', '44.227.217.144']
-port = int(os.environ.get('PORT', 4000))
+port = int(os.environ.get('PORT', 10000))
 
 while True:
     for host in hosts:
