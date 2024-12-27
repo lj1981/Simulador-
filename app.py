c685bb44bbb51640c21e17ede13ef222da2ebe51
Luiz Cunha - Maverick <97998057+lj1981@users.noreply.github.com>
2024-12-27 15:46:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-import time
+import os
 from flask import Flask
 
+# Inicializar o app Flask
 app = Flask(__name__)
-hosts = ['100.20.92.101', '44.255.181.72', '44.227.217.144']
-port = int(os.environ.get('PORT', 10000))
 
-while True:
-    for host in hosts:
-        app.run(host=host, port=port)
-        time.sleep(60)  # Pausar por 60 segundos antes de trocar de host
+# Rota principal para testar
+@app.route('/')
+def index():
+    return "Hello, Render! Your Flask app is running."
+
+# Ponto de entrada da aplicação
+if __name__ == '__main__':
+    # Render exige que o aplicativo escute na porta especificada pela variável PORT
+    port = int(os.getenv("PORT", 5000))  # Porta padrão 5000 caso não exista a variável de ambiente PORT
+    app.run(host='0.0.0.0', port=port)  # Host 0.0.0.0 permite conexões externas
+
