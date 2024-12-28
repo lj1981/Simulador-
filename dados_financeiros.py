2719c55a49eac8cb77eb4553be588b64b2dc4cc7
Luiz Cunha - Maverick <97998057+lj1981@users.noreply.github.com>
2024-12-28 09:33:54 -0300

    Update dados_financeiros.py

--- dados_financeiros.py
+++ dados_financeiros.py
@@ -10,7 +10,7 @@ Dados financeiros
 
 import pandas as pd
 import numpy as np
-from pandas_datareader import data
+#from pandas_datareader import data
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
