5e3017b45eb2971ee341c75bc9a6b250d6e4edf1
Luiz Cunha - Maverick <97998057+lj1981@users.noreply.github.com>
2024-12-28 17:54:40 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, Render!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use a variável PORT ou 5000 como fallback
+    app.run(host="0.0.0.0", port=port)
+
